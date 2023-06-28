7970dc15968c60f0b33801a023598ca664e2fade
Amoko Ivan <amokoivan@gmail.com>
2023-06-29 00:32:40 +0300

    Infinite scroll

--- src/pages/notifications/index.js
+++ src/pages/notifications/index.js
@@ -1,9 +1,42 @@
-import React from "react";
-import _ from "lodash";
+import React, { useState, useRef, useCallback } from "react";
 import NotificationItem from "../../components/notifications/notification";
 import Footer from "../../layouts/footer/footer";
+import usePosts from "../../hooks/usePosts";
 
-function index() {
+function Index() {
+  const [pageNum, setPageNum] = useState(1);
+
+  const { isLoading, isError, error, results, hasNextPage } = usePosts(pageNum);
+
+  const intObserver = useRef();
+
+  const lastPostRef = useCallback(
+    (post) => {
+      if (isLoading) return;
+
+      if (intObserver.current) intObserver.current.disconnect();
+
+      intObserver.current = new IntersectionObserver((posts) => {
+        if (posts[0].isIntersecting && hasNextPage) {
+          console.log("We are near the last post!");
+          setPageNum((prev) => prev + 1);
+        }
+      });
+
+      if (post) intObserver.current.observe(post);
+    },
+    [isLoading, hasNextPage]
+  );
+
+  if (isError) return <p className="center">Error: {error.message}</p>;
+
+  const content = results.map((post, i) => {
+    if (results.length === i + 1) {
+      return <NotificationItem ref={lastPostRef} key={post.id} notification={post} />;
+    }
+    return <NotificationItem key={post.id} notification={post} />;
+  });
+
   return (
     <div className="flex gap-5 pt-2 home-container">
       <div className="w-60">
@@ -12,9 +45,8 @@ function index() {
         </div>
       </div>
       <div className="w-1/2 mb-4 overflow-hidden bg-white border rounded-lg">
-        {_.times(20, (i) => (
-          <NotificationItem key={i} />
-        ))}
+        {content}
+        {isLoading && <p className="center">Loading More Posts...</p>}
       </div>
       <div className="w-80">
         <Footer />
@@ -23,4 +55,4 @@ function index() {
   );
 }
 
-export default index;
+export default Index;
