f2c7bc25006c5c44a154f417ed879410aa57d998
Amoko Ivan <amokoivan@gmail.com>
2023-06-28 16:26:21 +0300

    Messaging container

--- src/pages/chats/index.js
+++ src/pages/chats/index.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function index() {
   document.title = "Messaging";
-  return <div>Messaging Chat section</div>;
+  return (
+    <div className="bg-red-600 h-[calc(100vh_-_65px)] mt-2">
+      Messaging Chat section
+    </div>
+  );
 }
 
 export default index;
