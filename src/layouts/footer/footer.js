5583dfe359e1746206fd40633b3da9c5b467e14a
Amoko Ivan <amokoivan@gmail.com>
2023-06-28 19:55:05 +0300

    Advert image

--- src/layouts/footer/footer.js
+++ src/layouts/footer/footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="sticky mb-4 overflow-hidden top-16">
       <img
-        className="rounded-lg"
+        className="w-full bg-white rounded-lg"
         src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/travel-destination-ads-design-template-545e75ceaa0392805c6645f1f26e6d5d_screen.jpg?ts=1681799306"
         alt="advert"
       />
