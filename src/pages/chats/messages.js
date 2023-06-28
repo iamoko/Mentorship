99678e9c34fe447de932d6bd55ffa5b22c632672
Amoko Ivan <amokoivan@gmail.com>
2023-06-28 18:44:54 +0300

    Chat user details

--- src/pages/chats/messages.js
+++ src/pages/chats/messages.js
@@ -4,12 +4,23 @@ import { useSelector } from "react-redux";
 import { chats } from "../../services/reducers";
 
 function Messages() {
-  const title = useSelector(chats.chat).title;
+  const chat = useSelector(chats.chat);
   return (
     <div>
       <p className="px-4 py-3 text-sm font-semibold capitalize border-b">
-        {title.length === 0 ? "Messages section" : title}
+        {chat.title.length === 0 ? "Messages section" : chat.title}
       </p>
+
+      <div className="h-[calc(100vh_-_120px)] overflow-x-hidden overscroll-y-auto p-3">
+        <img
+          src={chat.picture}
+          alt="profile"
+          className="object-cover w-20 h-20 rounded-full min-w-20 min-h-20 max-w-20 max-h-20"
+        />
+        <div className="p-2">
+          <p className="text-lg font-semibold capitalize">{chat.title}</p>
+        </div>
+      </div>
     </div>
   );
 }
