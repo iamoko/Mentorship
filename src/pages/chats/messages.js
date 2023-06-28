48ab7dd08061c5e62de5baef4bf869bd91eb5a65
Amoko Ivan <amokoivan@gmail.com>
2023-06-28 19:09:37 +0300

    Chat message section

--- src/pages/chats/messages.js
+++ src/pages/chats/messages.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Message from "../../components/chat/message";
 
 import { chats } from "../../services/reducers";
 
@@ -8,21 +9,29 @@ function Messages() {
   return (
     <div>
       <p className="px-4 py-3 text-sm font-semibold capitalize border-b">
-        {chat.name.length === 0 ? "Messages section" : chat.name}
+        {chat.name !== undefined && chat.name.length > 0
+          ? chat.name
+          : "Messages section"}
       </p>
 
-      <div className="h-[calc(100vh_-_120px)] overflow-x-hidden overscroll-y-auto p-3">
-        <img
-          src={chat.picture}
-          alt="profile"
-          className="object-cover w-20 h-20 rounded-full min-w-20 min-h-20 max-w-20 max-h-20"
-        />
-        <div className="p-2">
-          <p className="text-lg font-semibold capitalize">{chat.name}</p>
+      {Object.keys(chat).length !== 0 && (
+        <div className="h-[calc(100vh_-_120px)] overflow-x-hidden overscroll-y-auto py-3">
+          <img
+            src={chat.picture}
+            alt="profile"
+            className="object-cover w-20 h-20 mx-3 rounded-full"
+            style={{ minWidth: "80px", maxWidth: "80px" }}
+          />
+          <div className="p-2 mx-3">
+            <p className="text-lg font-semibold capitalize">{chat.name}</p>
 
-          <p className="text-sm capitalize line-clamp-3">{chat.title}</p>
+            <p className="text-sm capitalize line-clamp-3">{chat.title}</p>
+          </div>
+
+          {/* Messages section */}
+          <Message message={chat} />
         </div>
-      </div>
+      )}
     </div>
   );
 }
